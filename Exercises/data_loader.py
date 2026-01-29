614973cf9a3cfb75aaf5be69cab3931e5ad3f401
SebastianP <sebastian.petersson@iths.se>
2026-01-29 11:44:43 +0100

    försökt implementera lazy loading

--- Exercises/data_loader.py
+++ Exercises/data_loader.py
@@ -1,30 +1,40 @@
 import numpy as np
 
 class DataLoader:
-    def __init__(self, doc):
+    def __init__(self, doc=r"../Data/measurements.txt"):
         self.doc = doc
         self.data = None
         self.headers = None
+        self._loaded = False
 
-    @classmethod 
-    def read_file(cls, filepath=None):
-        if filepath is None:
-            filepath = "../Data/measurements.txt"
-
-        return cls(filepath)
-    
-    def load_data(self):
-        with open(self.doc, "r") as f:
-            header_line = next(f).strip().lstrip("#").split()
-            self.headers = [h for h in header_line if not h.startswith("(")]
-
-            lines = [line.strip().split() for line in f if not line.startswith("#")]
-            self.data = np.array(lines, dtype=float)
+    def _load_data(self):
+        if not self._loaded:
+            print(">> Laddar filen nu (lazy load)...")
+            with open(self.doc, "r") as f:
+                header_line = next(f).strip().lstrip("#").split()
+                headers = []
+                skip_next=False
+                for i, h in enumerate(header_line):
+                    if skip_next:
+                        skip_next = False
+                        continue
+                    if i + 1 < len(header_line) and header_line[i+1].startswith("("):
+                        headers.append(h + header_line[i+1])
+                        skip_next = True
+                    else:
+                        headers.append(h)
+                self.headers = headers
+                
+                lines = [line.strip().split() for line in f if not line.startswith("#")]
+                self.data = np.array(lines, dtype= float)
+                self._loaded = True
 
     def get_rows(self):
+        self._load_data()
         return self.data
     
     def get_column(self, key):
+        self._load_data()
         if isinstance(key, int):
             return self.data[:, key]
         elif isinstance(key, str):
@@ -37,11 +47,13 @@ class DataLoader:
             raise TypeError("Key måste vara int (index) eller str (header-namn).")
     
     def get_header_index(self, header_name):
+        self._load_data()
         if self.headers is None:
             raise ValueError("Data ej laddat ännu.")
         return self.headers.index(header_name)
     
     def pretty_print(self, num_rows=5):
+        self._load_data()
         if self.headers is None or self.data is None:
             raise ValueError("Data ej laddat ännu")
         
@@ -56,6 +68,7 @@ class DataLoader:
 
     
 if __name__ == "__main__":
-    loader = DataLoader.read_file()
-    loader.load_data()
+    loader = DataLoader()
+    print(loader._loaded)
     loader.pretty_print(num_rows=10)
+    print(loader._loaded)
