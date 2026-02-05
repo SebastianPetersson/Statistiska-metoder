5c06f1f4154a5e7f99bf0223c118a017bfbb6bb8
SebastianP <sebastian.petersson@iths.se>
2026-02-05 14:40:25 +0100

    Nu sparas y_hat en gång istället för att den räknas ut i varje funktion. Även skapat docstrings för vissa funktioner

--- Labb/Linear_regression.py
+++ Labb/Linear_regression.py
@@ -5,18 +5,25 @@ class LinearRegression:
     def __init__(self, confidence_level=0.95):
         self.confidence_level = confidence_level
 
+        self.X = None
+        self.y = None
         self.b = None
         self.d = None
         self.n = None
-        self.X = None
-        self.y = None
+
         self.sigma2 = None
         self.C = None
+        self.y_hat = None
 
     def _check_fitted(self):
         if self.b is None or self.X is None or self.y is None:
             raise RuntimeError('Model must be fitted before calling this method.')
         
+    def y_hat(self):
+        if self.y_hat is None:
+            self.y_hat = self.X @ self.b
+        return self.y_hat
+    
     def fit(self, X, y):
 
         self.y = y
@@ -36,30 +43,58 @@ class LinearRegression:
 
         self.C = XtX_inv
 
+    def SSE(self):
+        """Returns sum of squared errors: sum((y - Xb)**2)"""
+        self._check_fitted()
+
+        SSE = np.sum((self.y - self.y_hat)**2)
+        return SSE
+    
+    def MSE(self):
+        """Returns the mean of squared errors (mean of SSE)"""
+        self._check_fitted()
+
+        residuals = self.y - self.y_hat
+        MSE = np.mean(residuals**2)
+        return MSE
+
+    def RMSE(self):
+        self._check_fitted()
+
+        RMSE = np.sqrt(self.MSE())
+        return RMSE
+    
     def var(self):
         self._check_fitted()
         
         n = self.n
         d = self.d
 
-        y_hat = self.X @ self.b
-        SSE = np.sum((self.y - y_hat)**2)
-        self.sigma2 = SSE / (n-d-1)
+        self.sigma2 = self.SSE() / (n-d-1)
 
         return self.sigma2
         
     def std(self):
         self._check_fitted()
 
-        sigma2 = self.sigma2
-        sigma = np.sqrt(sigma2)
+        if self.sigma2 is None:
+            self.var()
 
-        return sigma
+        return np.sqrt(self.sigma2)
     
-    def RMSE(self):
+    def significance(self):
         self._check_fitted()
 
-        if self.sigma2 == None:
-            self.var()
-            
-        return np.sqrt(self.sigma2)
+        d = self.d
+        n = self.n
+        
+        SSE = self.SSE()
+        y_mean = np.mean(self.y)
+        Syy = np.sum((self.y-y_mean)**2)
+        SSR = Syy - SSE
+
+        F = (SSR/d) / (SSE/(n-d-1))
+        p = st.f.sf(F, d, n-d-1)
+        return F, p
+
+        
