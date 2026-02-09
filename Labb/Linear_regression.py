350b844efdbabd18090398604d8d8480caa296b0
SebastianP <sebastian.petersson@iths.se>
2026-02-09 13:46:12 +0100

    Uppdaterat funktioner, lagt till r_squared, Syy och SSR

--- Labb/Linear_regression.py
+++ Labb/Linear_regression.py
@@ -4,25 +4,20 @@ import scipy.stats as st
 class LinearRegression:
     def __init__(self, confidence_level=0.95):
         self.confidence_level = confidence_level
-
         self.X = None
         self.y = None
         self.b = None
         self.d = None
         self.n = None
-
         self.sigma2 = None
         self.C = None
-        self.y_hat = None
+        self._y_hat = None
+        self.XtX_inv = None
 
     def _check_fitted(self):
         if self.b is None or self.X is None or self.y is None:
             raise RuntimeError('Model must be fitted before calling this method.')
-        
-    def y_hat(self):
-        if self.y_hat is None:
-            self.y_hat = self.X @ self.b
-        return self.y_hat
+
     
     def fit(self, X, y):
 
@@ -35,13 +30,20 @@ class LinearRegression:
         self.n = X_matrix.shape[0]
         self.d = X_matrix.shape[1] - 1
 
-        XtX = X_matrix.T @ X_matrix
-        XtX_inv = np.linalg.pinv(XtX)
-        XtY = X_matrix.T @ y
+        self.XtX_inv = np.linalg.pinv(X.T @ X)
+        self.b = self.XtX_inv @ (self.X.T @ self.y)
 
-        self.b = XtX_inv @ XtY
+        self.sigma2 = None
+        self.C = None
+        self._y_hat = None
+        self.y = y
 
-        self.C = XtX_inv
+    @property
+    def y_hat(self):
+        self._check_fitted()
+        if self._y_hat is None:
+            self._y_hat = (self.X @ self.b).reshape(-1)
+        return self._y_hat
 
     def SSE(self):
         """Returns sum of squared errors: sum((y - Xb)**2)"""
@@ -59,22 +61,38 @@ class LinearRegression:
         return MSE
 
     def RMSE(self):
+        """Returns Root of mean squared errors (RMSE)"""
         self._check_fitted()
 
         RMSE = np.sqrt(self.MSE())
         return RMSE
     
+    def Syy(self):
+        """Total sum of squares: sum((y - y_mean)^2)."""
+        self._check_fitted()
+        y_mean = np.mean(self.y)
+        Syy = np.sum((self.y - y_mean) **2)
+        return Syy
+
+    def SSR(self):
+        """Regression sum of squares: Syy -SSE"""
+        self._check_fitted()
+        SSR = self.Syy() - self.SSE()
+        return SSR
+    
     def var(self):
+        """Beräkna och spara unbiased estimator för sigma^2 och kovariansmatris C. Returns sigma2 (variance)."""
         self._check_fitted()
         
         n = self.n
         d = self.d
 
         self.sigma2 = self.SSE() / (n-d-1)
-
+        self.C = self.sigma2 * self.XtX_inv
         return self.sigma2
         
-    def std(self):
+    def residual_std(self):
+        """Returns standard deviation."""
         self._check_fitted()
 
         if self.sigma2 is None:
@@ -83,6 +101,7 @@ class LinearRegression:
         return np.sqrt(self.sigma2)
     
     def significance(self):
+        """Returns the significance of the regression. F and p values."""
         self._check_fitted()
 
         d = self.d
@@ -96,5 +115,34 @@ class LinearRegression:
         F = (SSR/d) / (SSE/(n-d-1))
         p = st.f.sf(F, d, n-d-1)
         return F, p
-
+    
+    def residuals(self):
+        r = self.y - self.y_hat
+        return r
+    
+    def confidence_interval(self, alpha=None):
+        """Evaluates the confidelce intervals for the regression. Input alpha between 0-1, standard is 0.95."""
         
+        if alpha is None:
+            alpha = 1 - self.confidence_level
+        else:
+            alpha = float(alpha)
+        if self.sigma2 is None:
+            self.var()
+        df = self.n - self.d -1
+        se = np.sqrt(np.diag(self.C))
+        t_crit = st.t.ppf(1 - alpha / 2, df)
+        
+        lower = self.b - t_crit * se
+        upper = self.b + t_crit * se
+
+        return np.column.stack([lower, upper])
+    
+    def r_squared(self):
+        self._check_fitted()
+
+        r_squared = self.SSR() / self.syy()
+        return r_squared
+    
+
+
